53dfce1ba1d6e5092948b5f04bcc545b8315dde5
hassaan4584 <hassaanfayyaz@gmail.com>
2014-12-15 11:58:57 +0500

    15-Dec-2014

---
+++ PhoneApp1/ListText.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneApp1
+{
+    class ListText
+    {
+        private string _myPhone;
+        private string _myName;
+
+        public string MyProperty
+        {
+            get { return _myName; }
+            set { _myName = value; }
+        }
+
+
+    }
+}
---
+++ PhoneApp1/Page1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace PhoneApp1
+{
+    public partial class Page1 : PhoneApplicationPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            var s = PhoneApplicationService.Current.State["name"];
+            String myName = s as string;
+            NextPageDisplayName.Text = myName;
+
+
+
+        }
+    }
+}
